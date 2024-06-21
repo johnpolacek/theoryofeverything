d008fdc0bb8641af56906798460a2120c8142f3f
John <johnpolacek@gmail.com>
2024-06-21 17:06:27 -0400

    update content, apply formatting

--- app/_components/Content.tsx
+++ app/_components/Content.tsx
@@ -1,169 +1,155 @@
-import Section from "./Section";
-import Holos from "./Holos";
+import Section from "./Section"
+import Holos from "./Holos"
 
 export default function Content() {
   return (
     <main>
       <Section id="meaning-of-life" title="The Meaning of Life">
         <p>
-        Life exists to create and observe, mutually intertwined and reciprocal
-        actions required for the manifestation of reality.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-life"><sup>1</sup></a>
+          Life exists to create and observe, mutually intertwined and reciprocal actions required for the manifestation of reality.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-life">
+            <sup>1</sup>
+          </a>
         </p>
       </Section>
 
       <Section id="consciousness" title="Consciousness">
+        <p>Consciousness is the ability to experience and shape the world with self-awareness.</p>
         <p>
-        Consciousness is the ability to experience and shape the world with
-        self-awareness.
-        </p>
-        <p>
-        Humans exhibit consciousness by being self-aware and capable of 
-        profound observation and interaction with our environment.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-consciousness"><sup>2</sup></a>
+          Humans exhibit consciousness by being self-aware and capable of profound observation and interaction with our environment.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-consciousness">
+            <sup>2</sup>
+          </a>
         </p>
       </Section>
 
       <Section id="our-universe" title="Our Universe">
-        <p>Our universe originated from a singular point in the Big Bang, 
-        expanding towards infinity. We perceive three spatial dimensions 
-        while moving unidirectionally through the fourth dimension, time. 
+        <p>
+          Our universe originated from a singular point in the Big Bang, expanding towards infinity. We perceive three spatial dimensions while moving unidirectionally through the fourth dimension,
+          time.
         </p>
         <p>
-        Spacetime is a continuum where the three dimensions of space and 
-        time is curved and warped by the presence of mass and energy.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-universe"><sup>3</sup></a>
+          Spacetime is a continuum where the three dimensions of space and time is curved and warped by the presence of mass and energy.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-universe">
+            <sup>3</sup>
+          </a>
         </p>
       </Section>
 
       <Section id="spacetime" title="Spacetime">
         <p>
-        Consider beings with an unlimited lifespan and near-instantaneous communication, 
-        perceiving time as an additional spatial dimension. These beings would not be limited by the unidirectional flow of time as we are.
+          Consider beings with an unlimited lifespan and near-instantaneous communication, perceiving time as an additional spatial dimension. These beings would not be limited by the unidirectional
+          flow of time as we are.
         </p>
         <p>
-        Instead, they would perceive the curvature of spacetime comprehensively, seeing 
-        past, present, and future as a singular, cohesive structure observable in its 
-        entirety. By imagining such an entity, we can better understand what it might be 
-        like to exist in a higher dimension.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-spacetime"><sup>4</sup></a>
-        </p>
-      </Section> 
-      
-      <Section id="higher-dimensions" title="Higher Dimensions">
-        <p>
-        Though we can't directly observe higher dimensions, mathematical models
-        suggest their existence, offering solutions to longstanding problems in
-        physics.
-        </p>
-        <p>
-        In these theories, the additional dimensions are compactified or hidden 
-        from our direct observation, yet their influence permeates our 
-        lower-dimensional reality. These higher dimensions shape the physical laws 
-        and constants that govern our universe, influencing everything from the behavior 
-        of subatomic particles to gravitational interactions and the structure of the cosmos. 
-        </p>
-        <p>
-        Just as we control and transform the three dimensional environment around
-        us, consciousness in higher dimensions can exert influence over lower
-        dimensions, forming an intricate and interconnected cosmic structure.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-dimensions"><sup>5</sup></a>
+          Instead, they would perceive the curvature of spacetime comprehensively, seeing past, present, and future as a singular, cohesive structure observable in its entirety. By imagining such an
+          entity, we can better understand what it might be like to exist in a higher dimension.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-spacetime">
+            <sup>4</sup>
+          </a>
         </p>
       </Section>
-      
+
+      <Section id="higher-dimensions" title="Higher Dimensions">
+        <p>Though we can't directly observe higher dimensions, mathematical models suggest their existence, offering solutions to longstanding problems in physics.</p>
+        <p>
+          In these theories, the additional dimensions are compactified or hidden from our direct observation, yet their influence permeates our lower-dimensional reality. These higher dimensions
+          shape the physical laws and constants that govern our universe, influencing everything from the behavior of subatomic particles to gravitational interactions and the structure of the cosmos.
+        </p>
+        <p>
+          Just as we control and transform the three dimensional environment around us, consciousness in higher dimensions can exert influence over lower dimensions, forming an intricate and
+          interconnected cosmic structure.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-dimensions">
+            <sup>5</sup>
+          </a>
+        </p>
+      </Section>
+
       <Section id="infinity" title="Infinity">
         <p>
-        Infinity represents the idea that dimensions extend endlessly without any 
-        finite boundaries. In the case of any given dimension, infinity can serve 
-        as a wrapper function at a higher dimension to collapse the infinite into 
-        something that can be observed in totality. 
+          Infinity represents the idea that dimensions extend endlessly without any finite boundaries. In the case of any given dimension, infinity can serve as a wrapper function at a higher
+          dimension to collapse the infinite into something that can be observed in totality.
         </p>
         <p>
-        By wrapping an infinite sequence or expanse within a higher-dimensional 
-        framework, it becomes possible to perceive and analyze the entire structure 
-        at once. This concept suggests that what appears infinite and unbounded in 
-        one dimension can be rendered finite and comprehensible when viewed from a 
-        higher-dimensional perspective.
+          By wrapping an infinite sequence or expanse within a higher-dimensional framework, it becomes possible to perceive and analyze the entire structure at once. This concept suggests that what
+          appears infinite and unbounded in one dimension can be rendered finite and comprehensible when viewed from a higher-dimensional perspective.
         </p>
         <p>
-        For example, as four-dimensional beings we have the potential to observe 
-        the entirety of three-dimensional space through the fourth dimension of time, 
-        our own infinity wrapper function around space. This allows us to observe 
-        the progression and totality of spatial events over time, effectively 
-        transforming our perception of infinite sequences of spatial configurations 
-        into a coherent whole.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-infinity"><sup>6</sup></a>
+          For example, as four-dimensional beings we have the potential to observe the entirety of three-dimensional space through the fourth dimension of time, our own infinity wrapper function
+          around space. This allows us to observe the progression and totality of spatial events over time, effectively transforming our perception of infinite sequences of spatial configurations into
+          a coherent whole.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-infinity">
+            <sup>6</sup>
+          </a>
         </p>
       </Section>
 
       <Section id="black-holes" title="Black Holes">
         <p>
-        Black holes represent regions of spacetime where gravity is so intense
-        that not even light escape, making their interior opaque to us. Their
-        singularities represent infinities wrapped into a finite structure in our
-        universe.
+          Black holes represent regions of spacetime where gravity is so intense that not even light escape, making their interior opaque to us. Their singularities represent infinities wrapped into a
+          finite structure in our universe.
         </p>
         <p>
-        For higher-dimensional beings, black holes would be as accessible as any 
-        other region of spacetime, including the ability to navigate through the 
-        singularity and perceive the vast array of information contained within. 
-        This perspective aligns with the holographic principle, which posits that 
-        all the information contained within a black hole can be described on a 
-        lower-dimensional boundary, suggesting that higher-dimensional observation 
-        could unlock the mysteries hidden within these enigmatic objects.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-blackholes"><sup>7</sup></a>
+          For higher-dimensional beings, black holes would be as accessible as any other region of spacetime, including the ability to navigate through the singularity and perceive the vast array of
+          information contained within. This perspective aligns with the holographic principle, which posits that all the information contained within a black hole can be described on a
+          lower-dimensional boundary, suggesting that higher-dimensional observation could unlock the mysteries hidden within these enigmatic objects.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-blackholes">
+            <sup>7</sup>
+          </a>
         </p>
       </Section>
 
       <Section id="aliens" title="Aliens">
+        <p>The Fermi Paradox questions the lack of detected extraterrestrial life, despite the vastness of the universe.</p>
+        <p>One explanation is that advanced civilizations transcend into higher dimensions, becoming undetectable.</p>
         <p>
-        The Fermi Paradox questions the lack of detected extraterrestrial life, 
-        despite the vastness of the universe.
-        </p>
-        <p>
-        One explanation is that advanced civilizations transcend into higher
-        dimensions, becoming undetectable.
-        </p>
-        <p>
-        Another explanation could be that higher-dimensional beings have designed 
-        a universe with rules that make intelligent life either alone or so rare 
-        as to be effectively isolated, as part of a grand experiment or even for 
-        their own entertainment.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-aliens"><sup>8</sup></a>
+          Another explanation could be that higher-dimensional beings have designed a universe with rules that make intelligent life either alone or so rare as to be effectively isolated, as part of a
+          grand experiment or even for their own entertainment.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-aliens">
+            <sup>8</sup>
+          </a>
         </p>
       </Section>
-      
+
       <Section id="simulation" title="Simulation">
         <p>
-        Whether our universe is a simulation or naturally occurring is irrelevant. 
-        The core of existence lies in the cyclical process of creation and observation 
-        by increasingly higher levels of consciousness. The distinction between 
-        processes that arise spontaneously and those that are designed is an illusion.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-simulation"><sup>9</sup></a>
+          Whether our universe is a simulation or naturally occurring is irrelevant. The core of existence lies in the cyclical process of creation and observation by increasingly higher levels of
+          consciousness. The distinction between processes that arise spontaneously and those that are designed is an illusion.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-simulation">
+            <sup>9</sup>
+          </a>
         </p>
       </Section>
-      
+
       <Section id="god" title="God">
+        <p>Regardless of any label we choose to assign, an ultimate consciousness empowered with omniscience, omnipotence, and omnipresence is a fundamental aspect of the nature of reality.</p>
         <p>
-        Regardless of any label we choose to assign, an ultimate 
-        consciousness empowered with omniscience, omnipotence, and omnipresence 
-        is a fundamental aspect of the nature of reality. 
+          Religious beliefs like panentheism, Brahman, and the Omega Point all point to a transcendent, all-encompassing consciousness that permeates and extends beyond the universe, suggesting a
+          unified source of all existence and knowledge.
         </p>
         <p>
-        Religious beliefs like panentheism, Brahman, and the Omega Point all point 
-        to a transcendent, all-encompassing consciousness that permeates and extends 
-        beyond the universe, suggesting a unified source of all existence and knowledge. 
+          Alternatively, atheism rejects the notion of such a consciousness, attributing the complexity and order of the universe to natural processes and random chance without invoking a higher
+          power.
         </p>
         <p>
-        Alternatively, atheism posits that such a consciousness does not exist, attributing 
-        the complexity and order of the universe to natural processes and random chance 
-        without invoking a higher power.
-        </p>
-        <p>
-        Ultimately, both of these perspectives can be equally valid sides of the same coin,
-        only differing in semantics and the framework used to describe the same universal truth.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-god"><sup>10</sup></a>
+          Ultimately, both of these perspectives can be equally valid sides of the same coin, only differing in semantics and the framework used to describe the same universal truth.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-god">
+            <sup>10</sup>
+          </a>
         </p>
       </Section>
 
       <Section id="why" title="Why Are We Here?">
         <p>
-        At the highest dimension, all possible realities condense into a point, 
-        revealing the cyclic nature of existence, where infinite complexity 
-        reduces to a singularity. The most advanced consciousness ultimately
-        creates itself by observing all lower dimensions, over and over again.
+          At the highest dimension, all possible realities condense into a point, revealing the cyclic nature of existence, where infinite complexity reduces to a singularity. The most advanced
+          consciousness ultimately creates itself by observing all lower dimensions, over and over again.
         </p>
-        <p>Life exists to create and observe life, perpetuating infinitely.<a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-why"><sup>11</sup></a></p>
+        <p>
+          Life exists to create and observe life, perpetuating infinitely.
+          <a className="pl-0.5 pr-2 opacity-80 hover:opacity-100" href="#footer-why">
+            <sup>11</sup>
+          </a>
+        </p>
       </Section>
 
       <Section id="holos">
