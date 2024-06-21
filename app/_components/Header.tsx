4dfc80323973d67db794fa81eb6515c13627e954
John <johnpolacek@gmail.com>
2024-06-21 19:32:10 -0400

    Update header and section title font sizes- Improve header and section title font sizes

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header id="home" className="relative p-8 lg:px-16 border-b border-black/20 border-dashed xl:h-[160px] animate pointer-events-none">
-      <h1 className="text-center -ml-2 xl:ml-0 xl:text-left text-7xl xl:text-8xl font-extralight xl:font-thin">
+      <h1 className="text-center scale-[0.8] sm:scale-90 md:scale-100 -ml-2 xl:ml-0 xl:text-left text-7xl xl:text-8xl font-extralight xl:font-thin">
         <sup className="text-5xl font-extralight relative -top-3 -mr-2 inline-block">a</sup> Theory <sup className="text-5xl font-extralight relative -top-3 -mr-2 inline-block">of</sup>{" "}
         <span className="tracking-wide">Everything</span>
       </h1>
--- app/_components/Section.tsx
+++ app/_components/Section.tsx
@@ -1,7 +1,7 @@
 export default function Section({ id, title, children }: { id: string; title?: string; children: React.ReactNode }) {
   return (
     <section id={id} className="flex flex-col gap-4 px-8 pt-12 pb-24 lg:px-16 border-b border-black/10 border-dashed last:border-b-0 animate">
-      {title && <h2 className="text-4xl sm:text-5xl font-light pb-4">{title}</h2>}
+      {title && <h2 className="text-3xl sm:text-4xl md:text-5xl font-light pb-4">{title}</h2>}
       <div className="max-w-[50rem] flex flex-col gap-6 text-lg text-black/80">{children}</div>
     </section>
   )
