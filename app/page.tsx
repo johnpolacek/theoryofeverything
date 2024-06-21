ab81363389bd1da5504ccbe5a60feb6982747b7c
John <johnpolacek@gmail.com>
2024-06-21 17:22:01 -0400

    Update component layout width in Home function

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Header from "./_components/Header";
-import Sidebar from "./_components/Sidebar";
-import Content from "./_components/Content";
+import Header from "./_components/Header"
+import Sidebar from "./_components/Sidebar"
+import Content from "./_components/Content"
 import Footer from "./_components/Footer"
 
 export default function Home() {
-	return (
-		<div className="xl:grid grid-cols-[1fr_5fr] justify-stretch w-full max-w-[2100px] overflow-hidden mx-auto bg-white shadow-xl">
-			<Sidebar />
-			<div className="grow h-screen overflow-auto">
-				<Header />
-				<Content />
-				<Footer />
-			</div>
-		</div>
-	);
+  return (
+    <div className="xl:grid grid-cols-[1fr_5fr] justify-stretch w-full max-w-[1440px] overflow-hidden mx-auto">
+      <Sidebar />
+      <div className="grow h-screen overflow-auto">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </div>
+  )
 }
