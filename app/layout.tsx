4bfe6adfc03c2c08ef8f6708313ba1fbcd60fd7d
John <johnpolacek@gmail.com>
2024-06-22 10:55:20 -0400

    add symbol to meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,26 @@
-import type { Metadata } from "next";
-import { Bitter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Bitter } from "next/font/google"
+import "./globals.css"
 
-const bitter = Bitter({ subsets: ["latin"] });
+const bitter = Bitter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "a Theory of Everything",
-  description: "a Theory of Everything - The Meaning of Life, Consciousness, Our Universe, Time, Higher Dimensions, Infinity, Black Holes, Aliens, Are We In a Simulation, Does God Exist, and... Why?",
+  description:
+    "⊛ a Theory of Everything - The Meaning of Life, Consciousness, Our Universe, Time, Higher Dimensions, Infinity, Black Holes, Aliens, Are We In a Simulation, Does God Exist, and... Why?",
   icons: {
     icon: "icon.svg",
-  }
-};
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={bitter.className}>{children}</body>
     </html>
-  );
+  )
 }
